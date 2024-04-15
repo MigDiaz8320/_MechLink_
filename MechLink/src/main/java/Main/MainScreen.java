41d474a046ebb2ce62177ee0fc776aa4ce3c5d1c
MD8320 <ps7320ka@go.minnstate.edu>
2024-04-15 17:20:06 -0500

    Added dependency to build.gradle for database, Added database creation, fixed up README.md

--- MechLink/src/main/java/Main/MainScreen.java
+++ MechLink/src/main/java/Main/MainScreen.java
@@ -17,6 +17,10 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class MainScreen extends Application {
     private MainScrenController controller ;
@@ -31,9 +35,25 @@ public class MainScreen extends Application {
         primaryStage.show();
     }
 
-    public static void main(String[] args) {
-        // Set the controller instance
+    public static void createNewDatabase(String fileName) {
 
+        String url = "jdbc:sqlite:" + fileName;
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                //System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        createNewDatabase("MechLink.db");
+        // Set the controller instance
         launch(args);
     }
 
