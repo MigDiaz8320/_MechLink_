aab55c14d7bf46bd6a21c901e9910ebacc5564d8
AhmedYu <abyan-adan@hotmail.com>
2024-04-18 22:41:15 -0500

    password confirmation label and textfield added

--- MechLink/src/main/java/View/SignUpView.java
+++ MechLink/src/main/java/View/SignUpView.java
@@ -6,106 +6,102 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
-import javafx.scene.control.ToggleGroup;
 
-import javafx.scene.control.RadioButton;
 import java.net.URL;
-import java.util.concurrent.Callable;
 
 public class SignUpView {
     private static SignUpView instance;
 
     private final Stage stage;
     private final Scene scene;
-    private TextField firstName, lastName, address, phoneNumber, zipCode, userName ;
-    private TextField emailTextField;
-    private PasswordField passwordField;
-    private Button createBTN;
+    private final TextField firstName, lastName, address, phoneNumber, zipCode, userName;
+    private final TextField emailTextField;
+    private final PasswordField passwordField, passwordConfirmation;
+    private final Button createBTN;
 
     private SignUpView() {
-
         // Create layout and add components
         GridPane gridPane = new GridPane();
-        gridPane.getStyleClass().add("signUpView");
-        gridPane.getStyleClass().add("text");
-
+        gridPane.getStyleClass().addAll("signUpView", "text");
         gridPane.setPadding(new Insets(10, 9, 0, 20));
         gridPane.setVgap(20);
         gridPane.setHgap(10);
+
         // Create UI components
-        // First & last name labels and text fields
         Label firstLabel = new Label("First name");
         firstName = new TextField();
-
 
         Label lastLabel = new Label("Last Name ");
         lastName = new TextField();
 
-        // Contact label
         Label contact = new Label("Contact");
         contact.setUnderline(true);
         contact.getStyleClass().add("title");
-        // Email,  ,
+
         Label emailLabel = new Label("Email:");
         emailTextField = new TextField();
-        //password
+
         Label passwordLabel = new Label("Password:");
         passwordField = new PasswordField();
-        // Phone label and text field
+
         Label phoneLabel = new Label("Phone:");
         phoneNumber = new TextField();
 
-        // Address label and text field
         Label addressLabel = new Label("Address:");
         address = new TextField();
 
+        Label passConfirmationLabel = new Label("Password Confirmation");
 
-Label zipCodeLabel = new Label("Zip-Code");
-TextField zipCode = new TextField();
-Label userNameLabel = new Label("User name ");
-TextField userNameTextField = new TextField();
+        Label zipCodeLabel = new Label("Zip-Code");
+        zipCode = new TextField();
 
+        Label userNameLabel = new Label("User name ");
+        userName = new TextField();
 
-Label secondTitle  = new Label("Finish signing up)");
-secondTitle.getStyleClass().add("title");
+        passwordConfirmation = new PasswordField();
+        passwordConfirmation.setPromptText("Re-enter your password!");
 
+        Label secondTitle = new Label("Finish signing up");
+        secondTitle.getStyleClass().add("title");
 
-        createBTN = new Button("create ");
+        createBTN = new Button("Create");
         createBTN.getStyleClass().add("loginBtn");
-        //ad nodes to the grid
-        gridPane.add(firstLabel, 0,0);
-        gridPane.add(firstName, 1,0);
+
+        // ToggleGroup for radio buttons
         ToggleGroup toggleGroup = new ToggleGroup();
-RadioButton rb1 = new RadioButton("Mechanic ");
-        RadioButton rb2 = new RadioButton("Client ");
+        RadioButton rb1 = new RadioButton("Mechanic");
+        RadioButton rb2 = new RadioButton("Client");
         rb1.setToggleGroup(toggleGroup);
         rb2.setToggleGroup(toggleGroup);
-        gridPane.add(lastLabel, 0,1);
+
+        // Add nodes to the grid
+        gridPane.add(firstLabel, 0, 0);
+        gridPane.add(firstName, 1, 0);
+        gridPane.add(lastLabel, 0, 1);
         gridPane.add(lastName, 1, 1);
-
-        gridPane.add(contact,0, 4);
-
-        gridPane.add(emailLabel,1, 5);
-        gridPane.add(emailTextField, 2,5);
-
-        gridPane.add(phoneLabel, 3,5);
-        gridPane.add(phoneNumber, 4,5);
+        gridPane.add(contact, 0, 4);
+        gridPane.add(emailLabel, 1, 5);
+        gridPane.add(emailTextField, 2, 5);
+        gridPane.add(phoneLabel, 3, 5);
+        gridPane.add(phoneNumber, 4, 5);
         gridPane.add(userNameLabel, 1, 6);
-        gridPane.add(userNameTextField, 2, 6);
-
-        gridPane.add(passwordLabel, 3,6);
+        gridPane.add(userName, 2, 6);
+        gridPane.add(passwordLabel, 3, 6);
         gridPane.add(passwordField, 4, 6);
-        gridPane.add(addressLabel, 1, 7);
-        gridPane.add(address, 2, 7);
-        gridPane.add(zipCodeLabel, 3, 7);
-        gridPane.add(zipCode, 4, 7);
+        gridPane.add(passConfirmationLabel, 3, 7);
+        gridPane.add(passwordConfirmation, 4, 7);
+        gridPane.add(addressLabel, 1, 8);
+        gridPane.add(address, 2, 8);
+        gridPane.add(zipCodeLabel, 3, 8);
+        gridPane.add(zipCode, 4, 8);
         gridPane.add(secondTitle, 0, 10);
         gridPane.add(rb1, 1, 11);
         gridPane.add(rb2, 1, 12);
-gridPane.add(createBTN, 4, 16);
-
+        gridPane.add(createBTN, 4, 16);
 
         // Create scene
         scene = new Scene(gridPane, 500, 700);
@@ -133,14 +129,6 @@ gridPane.add(createBTN, 4, 16);
         return scene;
     }
 
-    public TextField getEmailTextField() {
-        return emailTextField;
-    }
-
-    public PasswordField getPasswordField() {
-        return passwordField;
-    }
-
     public TextField getFirstName() {
         return firstName;
     }
@@ -161,14 +149,30 @@ gridPane.add(createBTN, 4, 16);
         return zipCode;
     }
 
-    public Button getcreateBTN() {
+    public TextField getUserName() {
+        return userName;
+    }
+
+    public TextField getEmailTextField() {
+        return emailTextField;
+    }
+
+    public PasswordField getPasswordField() {
+        return passwordField;
+    }
+
+    public PasswordField getPasswordConfirmation() {
+        return passwordConfirmation;
+    }
+
+    public Button getCreateBTN() {
         return createBTN;
     }
 
     private void loadStylesheetIntoScene(Scene scene) {
         URL stylesheetURL = getClass().getResource("/mainStyle.css");
         if (stylesheetURL == null) {
-            System.out.println("the style sheet was null!!!!");
+            System.out.println("The style sheet was null!");
             return;
         }
         scene.getStylesheets().add(stylesheetURL.toExternalForm());
